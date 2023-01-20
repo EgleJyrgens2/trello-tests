b0082a7ee30b109c89b746d78eb6c9ba5419b21d
GaohengWu <gaoheng.wu@gmail.com>
2023-01-20 11:47:57 +0200

    I changed the board name and board url

--- cypress/e2e/trello-open-main.pageGao.cy.js
+++ cypress/e2e/trello-open-main.pageGao.cy.js
@@ -2,12 +2,12 @@ import BoardPage from "../pages/BoardPage";
 import Boards from "../pages/Boards";
 import LoginPage from "../pages/LoginPage";
 
-describe('Test of Gao on Trello board', () => {
+describe('Test on Trello board', () => {
 
     //This board was created before manually through the UI
     //those constants will be different for each user
-    const myKanbanBoard = 'Kanban board - CerebrumHub';
-    const myKanbanBoardUrl = 'https://trello.com/b/i6yPZ6Kn/kanban-board-cerebrumhub';
+    const myKanbanBoard = 'Gao Board';
+    const myKanbanBoardUrl = 'https://trello.com/b/5mqsWnW0/gao-board';
 
     beforeEach(() => {
         LoginPage.openTrelloLoginPage();
