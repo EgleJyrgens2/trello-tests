cc29f2abcdb4b8ce74925dc1cae5df3a5c31f384
EgleJyrgens2 <egle.jyrgens@gmail.com>
2023-01-21 13:25:19 +0200

    workshop 38 hide menu bar on the left

--- cypress/e2e/trello-open-main.pageEgle.cy.js
+++ cypress/e2e/trello-open-main.pageEgle.cy.js
@@ -23,7 +23,32 @@ describe('Test on Trello board', () => {
         BoardPage.boardUrlIsCorrect(myKanbanBoardUrl);
     });
 
-    it.skip('Some more tests', () => {
-       //add more tests
+    it('Hide menu bar on the left (minimize)', () => {
+        cy.get('[data-testid="workspace-boards-and-views-lists"]').should('be.visible');
+        cy.get('[data-testid="workspace-navigation-collapse-button"]').click();
+        cy.get('[data-testid="workspace-boards-and-views-lists"]').should('not.be.visible');
+        cy.get('[data-testid="workspace-navigation-expand-button"]').click();
+        cy.get('[data-testid="workspace-boards-and-views-lists"]').should('be.visible');
+    });
+
+    it('Edit board name and change it back', () => {
+        cy.get('.js-board-editing-target').type('something');
+        cy.get('.board-header').click();
+        cy.get('.js-board-editing-target').should('have.text', 'something');
+        cy.get('.js-board-editing-target').type('Egle Board');
+        cy.get('.board-header').click();
+        cy.get('.js-board-editing-target').should('contain', myKanbanBoard);
+    });
+
+    it('Star and unstar the board', () => {
+        cy.get('.board-header-star-container .icon-star').should('be.visible').and('have.css', 'color','rgb(23, 43, 77)');
+        cy.get('.board header-star-container .icon-starred').should('not.exist');
+        cy.get('.board-header-star-container .icon-star').click();
+        cy.get('.board-header-star-container .icon-star').should('not.exist');
+        cy.get('.board-header-star-container .icon-starred').should('be.visible').and('have.css', 'color', 'rgb(242, 214, 0)');
+        cy.get('.board-header-star-container .icon-starred').click();
+        cy.get('.board-header-star-container .icon-starred').should('not.exist');
+        cy.get('.board-header-star-container .icon-star').should('be.visible').and('have.css','color','rgb(23, 43, 77)');
     });
 });
+
