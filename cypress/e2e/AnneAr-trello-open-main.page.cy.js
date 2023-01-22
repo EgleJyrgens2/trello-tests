59a9af315bcf095222ea5f873fee6f06851b583d
annarees <annarees9@gmail.com>
2023-01-22 10:36:34 +0200

    Updated 22012023

--- cypress/e2e/AnneAr-trello-open-main.page.cy.js
+++ cypress/e2e/AnneAr-trello-open-main.page.cy.js
@@ -1,5 +1,5 @@
-import BoardPage from "../pages/BoardPage";
-import Boards from "../pages/Boards";
+import BoardPage from "../pages/AnneBoardPage";
+import Boards from "../pages/AnneBoards";
 import LoginPage from "../pages/LoginPage";
 
 describe('Test on Trello board', () => {
@@ -22,7 +22,16 @@ describe('Test on Trello board', () => {
         BoardPage.boardUrlIsCorrect(myKanbanBoardUrl);
     });
 
-    it.skip('Some more tests', () => {
-       //add more tests
+    it('Edit board name and change it back', () => {
+        cy.get('h1.js-board-editing-target').should('be.visible');
+        cy.get('h1.js-board-editing-target').should('have.text','Anne Board');
+        cy.get('h1.js-board-editing-target').click();
+        //cy.get('h1.js-board-editing-target').next().type(myKanbanBoard + 'updated{enter}');
+        cy.get('.js-rename-board').type('muudetud{enter}');
+        cy.get('.js-rename-board').should('have.text','muudetud');
+        cy.get('.js-rename-board').click();
+        cy.get('.js-rename-board').type('Anne Board{enter}');
+        cy.get('h1.js-board-editing-target').should('have.text','Anne Board');
+
     });
 });
