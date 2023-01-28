3d557b88325d12b972ebaa45c700ffada5d5bdbd
annarees <annarees9@gmail.com>
2023-01-28 10:20:43 +0200

    Updated 28012023

--- cypress/e2e/AnneAr-trello-open-main.page.cy.js
+++ cypress/e2e/AnneAr-trello-open-main.page.cy.js
@@ -34,4 +34,27 @@ describe('Test on Trello board', () => {
         cy.get('h1.js-board-editing-target').should('have.text','Anne Board');
 
     });
+    it('Change background color', () => {
+        cy.get('[aria-label="Show menu"]');//.click();
+        cy.get('.js-change-background').click();
+        cy.get('.js-bg-colors').contains('Colors').click();
+        cy.get('.image').eq(7).click(); //changing to turquoise
+        cy.get('.board-menu-header-close-button').click();
+      
+    });
+
+    it('Rename list (column)', () => {
+        cy.get('.list-header-target').eq(0).should('be.visible').click();
+        cy.get('.js-list-name-input').eq(0).type('Muudetud nimetus{enter}');
+        cy.get('.js-list-name-assist').eq(0).should('have.text','Muudetud nimetus');
+    
+    }); 
+    it('Copy list (column)', () => {
+        cy.get('.list-header-extras').eq(0).should('be.visible').click();
+        cy.get('.pop-over-header-title');
+        cy.get('.js-copy-list').click();
+        cy.get('textarea.js-autofocus').click().type('New List{enter}');
+        cy.get('.list-header-extras').eq(1).should('be.visible');
+    
+    }); 
 });
